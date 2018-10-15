312eb0bf4f55fbb4541694c60e8fe28d065f3c89
nelvinProjects <Samsung18/>
2018-10-15 16:53:03 +0100

    Fibonacci done

--- src/main/scala/Fibonacci.scala
+++ src/main/scala/Fibonacci.scala
@@ -6,6 +6,12 @@
 object Fibonacci {
   def main(args: Array[String]): Unit = {
     fibonacci(10)
+    fibonacci2(0)
+    fibonacci2(1)
+    fibonacci2(2)
+    fibonacci2(3)
+    fibonacci2(4)
+    fibonacci2(5)
   }
 
   /**
@@ -21,5 +27,17 @@ object Fibonacci {
       fibonacci(nth - 2, 1, 0)
     case _ => println(i + previous)
       fibonacci(nth - 1, i + previous, i)
+  }
+
+  /**
+    * Print fibonacci at given value
+    *
+    * @param valueNeeded how many fibonacci to until
+    * @param i           the current fibonacci at current
+    * @param previous    last fibonacci
+    */
+  def fibonacci2(valueNeeded: Int, i: Int = 0, previous: Int = 1): Unit = valueNeeded match {
+    case value if valueNeeded == 0 => println(i)
+    case _ => fibonacci2(valueNeeded - 1, i + previous, i)
   }
 }
